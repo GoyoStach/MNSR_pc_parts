4e33a8cf2789f187a1ec3fb29b83f511f7cc3503
GoyoStach <guillaume.excoffier86@gmail.com>
2025-08-08 18:11:40 +0200

    Adding standalone config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  adapter: node({
+    mode: 'standalone'
+  })
 });
